723782a7e84c9802eacc5d9d35c33e1338d88601
guajardo <a282057@uach.mx>
2018-05-15 12:33:41 -0600

    Add student feature specs

---
+++ spec/features/students_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.feature "Students", type: :feature do
+  scenario 'user creates a student' do
+    visit new_student_path
+
+    expect {
+      fill_in 'Name', with: 'Javier Guajardo'
+      fill_in 'Email', with: 'f.javi.guajardo@gmail.com'
+      fill_in 'Phone number', with: '(636) 109 - 3561'
+      fill_in 'Date of birth', with: '1994-02-02'
+      fill_in 'Graduation date', with: '2017-05-30'
+      click_button 'Submit'
+    }.to change(Student, :count).by(1)
+
+    aggregate_failures do
+      expect(page).to have_content 'Student was successfully created'
+      expect(page).to have_content 'Javier Guajardo'
+      expect(page).to have_content 'f.javi.guajardo@gmail.com'
+      expect(page).to have_content '(636) 109 - 3561'
+      expect(page).to have_content 'February 2, 1994'
+      expect(page).to have_content 'May 30, 2017'
+    end
+  end
+
+  scenario 'user edits a student' do
+    student = FactoryGirl.create(:student)
+
+    visit edit_student_path(student.id)
+
+    fill_in 'Name', with: 'Tim Jones'
+    click_button 'Submit'
+
+    expect(student.reload.name).to eq('Tim Jones')
+  end
+
+  scenario 'user deletes a student' do
+    Student.create!(FactoryGirl.attributes_for(:student))
+
+    visit students_path
+
+    expect {
+      find(:css, '.btn-danger').click
+    }.to change(Student, :count).by(-1)
+
+    expect(page).to have_content 'Student was successfully destroyed'
+  end
+end
